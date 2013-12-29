678ab7be7bc0f0c721054979ff1fdb56a5988687
Adam Stokes <adam.stokes@ubuntu.com>
2013-12-29 18:33:59 -0500

    dont load templates from plugins
    Signed-off-by: Adam Stokes <adam.stokes@ubuntu.com>

--- lib/App/skryf/Plugin/Blog.pm
+++ lib/App/skryf/Plugin/Blog.pm
@@ -18,13 +18,6 @@ has namespace       => 'App::skryf::Plugin::Blog::Controller';
 sub register {
     my ($self, $app, $config) = @_;
 
-    my $base = catdir(dirname(__FILE__), 'Blog');
-    push @{$app->renderer->paths}, catdir($base, 'templates');
-    push @{$app->static->paths},   catdir($base, 'public');
-
-    push @{$app->renderer->classes}, __PACKAGE__;
-    push @{$app->static->classes},   __PACKAGE__;
-
     $app->routes->route($self->feedPath)->via('GET')->to(
         namespace => $self->namespace,
         action    => 'blog_feeds',
