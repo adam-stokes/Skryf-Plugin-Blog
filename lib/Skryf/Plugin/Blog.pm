29dfd2c187a3b3b17dbc73fac8cce04970ec39f4
Adam Stokes <adam.stokes@ubuntu.com>
2014-01-13 14:35:04 -0500

    0.03

--- lib/Skryf/Plugin/Blog.pm
+++ lib/Skryf/Plugin/Blog.pm
@@ -6,7 +6,7 @@ use Mojo::JSON;
 use Skryf::Plugin::Blog::Model;
 use Skryf::Util;
 
-our $VERSION = '0.02';
+our $VERSION = '0.03';
 
 ###############################################################################
 # Plugin Metadata
